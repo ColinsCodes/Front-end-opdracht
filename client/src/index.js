5f4bcbd7c5ed7617f072d699e728d03141d1994e
Colin Kasteleijn <ckasteleijn@sogyo.nl>
2026-08-11 17:06:44 +0200

    opdracht 2.5 af

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,59 @@
 function orderButtonClicked(event) {
     const orderParent = event.target.closest(".order");
+    const ticketsAdults = Math.max(0, Number(orderParent.querySelector(".numberofadults")?.value)) || 0;
+    const ticketsKids = Math.max(0, Number(orderParent.querySelector(".numberofkids")?.value)) || 0;
     const attraction = orderParent.parentNode.querySelector(".parkname").innerHTML;
-    const ticketsAdult = Number(orderParent.querySelector(".numberofadults").value) || 0;
-    const ticketsKids = Number(orderParent.querySelector(".numberofkids").value) || 0;
+    const orderTotal = tallyOrder(orderParent)
+
+    const cartItem = {
+        attractionName: attraction,
+        adultTickets: ticketsAdults,
+        kidTickets: ticketsKids,
+        priceTotal: orderTotal
+    };
+    if (orderTotal) {
+        saveOrderInCart(cartItem);
+    }
+    orderParent.querySelector(".numberofadults").value = 0;
+    orderParent.querySelector(".numberofkids").value = 0;
+    orderParent.querySelector(".total .price").innerHTML = "0,-";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const allInputs = document.querySelectorAll('.numberofadults, .numberofkids');
+    allInputs.forEach(input => {
+        input.addEventListener('input', calculateLiveTotal);
+    });
+});
+
+
+function calculateLiveTotal(event) {
+    const orderParent = event.target.closest(".order");
+    orderParent.querySelector(".total .price").innerHTML = tallyOrder(orderParent) +",-";
+}
+
+function tallyOrder(orderParent) {
+    const ticketsAdult = Math.max(0, Number(orderParent.querySelector(".numberofadults")?.value)) || 0;
+    const ticketsKids = Math.max(0, Number(orderParent.querySelector(".numberofkids")?.value)) || 0;
     const priceAdults = parseInt(orderParent.querySelector(".prices .adultprice .price").innerHTML);
     const priceKids = parseInt(orderParent.querySelector(".prices .kidsprice .price").innerHTML);
-    orderParent.querySelector(".total .price").innerHTML = priceAdults*ticketsAdult+priceKids*ticketsKids + ",-";
+
+    let doublesAdults = Math.floor(ticketsAdult / 2);
+    let doublesKids = Math.floor(ticketsKids / 2);
+    let totalDoubles = Math.min(doublesAdults, doublesKids);
+    let priceTotal = 0
+
+    doublesAdults -= totalDoubles;
+    doublesKids -= totalDoubles;
+    priceTotal += (totalDoubles*(2*(priceKids+priceAdults)*0.85))
+    priceTotal += (2*doublesKids*priceKids + 2*doublesAdults*priceAdults)
+    const priceRemainder = (ticketsAdult % 2) * priceAdults + (ticketsKids % 2) * priceKids
+    priceTotal += priceRemainder
+    return priceTotal;
+}
+
+function saveOrderInCart(cartItem) {
+    const currentOrders = JSON.parse(localStorage.getItem("orders")) || [];
+    currentOrders.push(cartItem);
+    localStorage.setItem("orders", JSON.stringify(currentOrders))
 }
